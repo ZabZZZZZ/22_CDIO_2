1c16d156f07b799b882b1287cde18065f2801628
Peterpedalum <ekpd@live.dk>
2023-10-24 10:54:47 +0200

    player class

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+class Player {
+
+    public String name;
+    public int points;
+
+
+
+    public Player (String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    public String toString() {
+
+        return this.name + " har " + this.points + " points.";
+    }
+
+
+
+}
+
